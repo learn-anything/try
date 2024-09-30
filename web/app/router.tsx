673178d0cc5c32b3c135453df4b3e9a6fb82dd9e
Aslam H <iupin5212@gmail.com>
2024-10-01 01:08:38 +0700

    fix: routerWithQueryClient

--- web/app/router.tsx
+++ web/app/router.tsx
@@ -2,14 +2,22 @@ import { createRouter as createTanStackRouter } from "@tanstack/react-router"
 import { routeTree } from "./routeTree.gen"
 import { DefaultCatchBoundary } from "./components/DefaultCatchBoundary"
 import { NotFound } from "./components/NotFound"
+import { QueryClient } from "@tanstack/react-query"
+import { routerWithQueryClient } from "@tanstack/react-router-with-query"
 
 export function createRouter() {
-  const router = createTanStackRouter({
-    routeTree,
-    defaultPreload: "intent",
-    defaultErrorComponent: DefaultCatchBoundary,
-    defaultNotFoundComponent: () => <NotFound />,
-  })
+  const queryClient = new QueryClient()
+
+  const router = routerWithQueryClient(
+    createTanStackRouter({
+      routeTree,
+      defaultPreload: "intent",
+      defaultErrorComponent: DefaultCatchBoundary,
+      defaultNotFoundComponent: () => <NotFound />,
+      context: { queryClient },
+    }),
+    queryClient,
+  )
 
   return router
 }
