32e1b9dc91415cf98053c8d60d5d9370430b957c
Nikita <github@nikiv.dev>
2024-11-26 17:52:17 +0100

    lets try another template

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
+  content: ['./App.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+
+  presets: [require('nativewind/preset')],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
